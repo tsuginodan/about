9b2d36689b782174a3c4925b6966cba6743865e0
Daniel Alvarez Vera <akaisuicchi@gmail.com>
2020-09-17 20:25:39 -0500

    0.0.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,14 @@
-// https://github.com/tailwindcss/tailwindcss/issues/654
-const makeNeumorphism = (name, lightRgb, shadowRgb, distance = 5) => {
-  const shadows = {};
-  const intensity = distance * 3;
-
-  shadows[
-    `${name}--top-left`
-  ] = `${distance}px ${distance}px ${intensity}px ${shadowRgb}, ${distance * -1}px ${distance *
-    -1}px ${intensity}px ${lightRgb}`;
-  shadows[
-    `${name}--pressed--top-left`
-  ] = `inset ${distance}px ${distance}px ${intensity}px ${shadowRgb}, inset ${distance *
-    -1}px ${distance * -1}px ${intensity}px ${lightRgb}`;
-
-  shadows[`${name}--top-right`] = `${distance *
-    -1}px ${distance}px ${intensity}px ${shadowRgb}, ${distance}px ${distance *
-    -1}px ${intensity}px ${lightRgb}`;
-  shadows[`${name}--pressed--top-right`] = `inset ${distance *
-    -1}px ${distance}px ${intensity}px ${shadowRgb}, inset ${distance}px ${distance *
-    -1}px ${intensity}px ${lightRgb}`;
-
-  shadows[`${name}--bottom-left`] = `${distance}px ${distance *
-    -1}px ${intensity}px ${shadowRgb}, ${distance * -1}px ${distance}px ${intensity}px ${lightRgb}`;
-  shadows[`${name}--pressed--bottom-left`] = `inset ${distance}px ${distance *
-    -1}px ${intensity}px ${shadowRgb}, inset ${distance *
-    -1}px ${distance}px ${intensity}px ${lightRgb}`;
-
-  shadows[`${name}--bottom-right`] = `${distance * -1}px ${distance *
-    -1}px ${intensity}px ${shadowRgb}, ${distance}px ${distance}px ${intensity}px ${lightRgb}`;
-  shadows[`${name}--pressed--bottom-right`] = `inset ${distance * -1}px ${distance *
-    -1}px ${intensity}px ${shadowRgb}, inset ${distance}px ${distance}px ${intensity}px ${lightRgb}`;
-
-  return shadows;
-};
-
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
   purge: {
     content: [ './src/**/*.html' ],
   },
   theme: {
-    extend: {
-      boxShadow: {
-        ...makeNeumorphism('lg', '#90cdf4', '#4299e1', 20), // name - color below used - color above used
-        ...makeNeumorphism('sm', '#90cdf4', '#4299e1', 10), // name - color below used - color above used
-      },
+    fontFamily: {
+      display: [ 'Grandstander' ],
     },
   },
   variants: {},
